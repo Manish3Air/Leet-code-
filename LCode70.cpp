932088740c3749d7223af677748fe3ae389285f4
Manish3air <manishpandey3365@gmail.com>
2024-06-03 15:31:09 +0530

    Updated to Tabulation format of DP

--- LCode70.cpp
+++ LCode70.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    int totalnoofways(int n,vector<int> &dp){
-    if(n==0) return 1;
-    if(n==1) return 1;
-    if(dp[n]!=-1) return dp[n];
-        int left=totalnoofways(n-1,dp);
-        int right=totalnoofways(n-2,dp);
-        return dp[n]=(left+right);
-    }
+    // int totalnoofways(int n,vector<int> &dp){
+    // if(n==0) return 1;
+    // if(n==1) return 1;
+    // if(dp[n]!=-1) return dp[n];
+    //     int left=totalnoofways(n-1,dp);
+    //     int right=totalnoofways(n-2,dp);
+    //     return dp[n]=(left+right);
+    // }
+    // int climbStairs(int n) {
+    // vector<int> dp(n+1,-1);
+    //     return totalnoofways(n,dp);
+    // }
+
     int climbStairs(int n) {
-    vector<int> dp(n+1,-1);
-        return totalnoofways(n,dp);
+    if (n <= 1) {
+        return 1;
+    }
+    vector<int> dp(n + 1, -1);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
     }
 };
 
